902ad99ecd63775b8c910af403afb2d33b25555f
yuxfzju <243915316@qq.com>
2016-07-24 23:40:51 +0800

    Box Blur

---
+++ app/src/main/java/com/example/xiangpi/dynamicblurdemo/BoxBlur.java
@@ -0,0 +1,138 @@
+package com.example.xiangpi.dynamicblurdemo;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+
+/**
+ * Created by 余晓飞 on 2016/7/24.
+ */
+/*
+ * The Box Blur code below is part the BlurTestAndroid project by patrickfav (https://github.com/patrickfav)
+ *
+ * https://github.com/patrickfav/BlurTestAndroid/blob/master/BlurBenchmark/src/main/java/at/favre/app/blurbenchmark/blur/algorithms/BoxBlur.java
+ *
+ * The box blur algorithm was implemented using this post http://stackoverflow.com/questions/8218438/android-box-blur-algorithm
+ *
+ */
+public class BoxBlur {
+
+    public static Bitmap blur(int radius, Bitmap bmp) {
+        assert (radius & 1) == 0 : "Range must be odd.";
+
+        Bitmap blurred = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(blurred);
+
+        int w = bmp.getWidth();
+        int h = bmp.getHeight();
+
+        int[] pixels = new int[bmp.getWidth() * bmp.getHeight()];
+        bmp.getPixels(pixels, 0, w, 0, 0, w, h);
+
+        boxBlurHorizontal(pixels, w, h, radius / 2);
+        boxBlurVertical(pixels, w, h, radius / 2);
+
+        c.drawBitmap(pixels, 0, w, 0.0F, 0.0F, w, h, true, null);
+
+        return blurred;
+    }
+
+    private static void boxBlurHorizontal(int[] pixels, int w, int h,
+                                          int halfRange) {
+        int index = 0;
+        int[] newColors = new int[w];
+
+        for (int y = 0; y < h; y++) {
+            int hits = 0;
+            long r = 0;
+            long g = 0;
+            long b = 0;
+            for (int x = -halfRange; x < w; x++) {
+                int oldPixel = x - halfRange - 1;
+                if (oldPixel >= 0) {
+                    int color = pixels[index + oldPixel];
+                    if (color != 0) {
+                        r -= Color.red(color);
+                        g -= Color.green(color);
+                        b -= Color.blue(color);
+                    }
+                    hits--;
+                }
+
+                int newPixel = x + halfRange;
+                if (newPixel < w) {
+                    int color = pixels[index + newPixel];
+                    if (color != 0) {
+                        r += Color.red(color);
+                        g += Color.green(color);
+                        b += Color.blue(color);
+                    }
+                    hits++;
+                }
+
+                if (x >= 0) {
+                    newColors[x] = Color.argb(0xFF, (int) (r / hits), (int) (g / hits), (int) (b / hits));
+                }
+            }
+
+            System.arraycopy(newColors, 0, pixels, index + 0, w);
+
+            index += w;
+        }
+    }
+
+    private static void boxBlurVertical(int[] pixels, int w, int h,
+                                        int halfRange) {
+
+        int[] newColors = new int[h];
+        int oldPixelOffset = -(halfRange + 1) * w;
+        int newPixelOffset = (halfRange) * w;
+
+        for (int x = 0; x < w; x++) {
+            int hits = 0;
+            long r = 0;
+            long g = 0;
+            long b = 0;
+            int index = -halfRange * w + x;
+            for (int y = -halfRange; y < h; y++) {
+                int oldPixel = y - halfRange - 1;
+                if (oldPixel >= 0) {
+                    int color = pixels[index + oldPixelOffset];
+                    if (color != 0) {
+                        r -= Color.red(color);
+                        g -= Color.green(color);
+                        b -= Color.blue(color);
+                    }
+                    hits--;
+                }
+
+                int newPixel = y + halfRange;
+                if (newPixel < h) {
+                    int color = pixels[index + newPixelOffset];
+                    if (color != 0) {
+                        r += Color.red(color);
+                        g += Color.green(color);
+                        b += Color.blue(color);
+                    }
+                    hits++;
+                }
+
+                if (y >= 0) {
+                    newColors[y] = Color.argb(0xFF, (int) (r / hits), (int) (g / hits), (int) (b / hits));
+                }
+
+                index += w;
+            }
+
+            for (int y = 0; y < h; y++) {
+                pixels[y * w + x] = newColors[y];
+            }
+        }
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/example/xiangpi/dynamicblurdemo/MainActivity.java
+++ app/src/main/java/com/example/xiangpi/dynamicblurdemo/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.xiangpi.dynamicblurdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageView mImageView;
+    private Button mBlurBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.photo);
+        mBlurBtn = (Button) findViewById(R.id.blur_btn);
+
+        mImageView.setImageResource(R.mipmap.sample1);
+
+        mBlurBtn.setOnClickListener(this);
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.sample1);
+
+                final int w = bitmap.getWidth();
+                final int h = bitmap.getHeight();
+
+                int[] pixels = new int[w * h];
+                bitmap.getPixels(pixels, 0, w, 0, 0, w, h);
+
+                final long start = System.currentTimeMillis();
+
+                final Bitmap blurred = BoxBlur.blur(10, bitmap);
+                final long stop = System.currentTimeMillis();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mImageView.setImageBitmap(blurred);
+                        Toast.makeText(MainActivity.this, (stop - start) + "ms", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }).start();
     }
 }
