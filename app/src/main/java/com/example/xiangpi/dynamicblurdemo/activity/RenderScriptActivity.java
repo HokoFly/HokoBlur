a981b613ab1552061d5f4427cae425cc12fc81a4
yuxfzju <243915316@qq.com>
2016-08-01 21:46:00 +0800

    Boxblur的RenderScript实现

--- app/src/main/java/com/example/xiangpi/dynamicblurdemo/activity/RenderScriptActivity.java
+++ app/src/main/java/com/example/xiangpi/dynamicblurdemo/activity/RenderScriptActivity.java
@@ -13,18 +13,28 @@ import android.widget.Toast;
 
 import com.example.xiangpi.dynamicblurdemo.R;
 
-public class RenderScriptActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+public class RenderScriptActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final float BLUR_KERNEL_RADIUS = 10f;
 
     private RenderScript mRenderScript;
+
+    private Bitmap mBitmapIn;
+    private Bitmap mBitmapOut;
 
     private Allocation mAllocationIn;
     private Allocation mAllocationOut;
 
-    private ScriptC_invert mScript;
+    private ScriptC_invert mScriptInvert;
     private ScriptIntrinsicBlur mScriptBlur;
+    private ScriptC_boxblur mScriptBoxBlur;
 
     private Button mInvertBtn;
-    private Button mBlurBtn;
+    private Button mBoxBlurBtn;
+    private Button mStackBlurBtn;
+    private Button mGaussianBlurBtn;
 
     private ImageView mImageView;
 
@@ -34,66 +44,29 @@ public class RenderScriptActivity extends AppCompatActivity {
         setContentView(R.layout.activity_render_script);
 
         mInvertBtn = (Button) findViewById(R.id.invert_btn);
-        mBlurBtn = (Button) findViewById(R.id.blur_btn);
+        mBoxBlurBtn = (Button) findViewById(R.id.box_blur_btn);
+        mStackBlurBtn = (Button) findViewById(R.id.stack_blur_btn);
+        mGaussianBlurBtn = (Button) findViewById(R.id.gaussian_blur_btn);
         mImageView = (ImageView) findViewById(R.id.photo);
 
         mRenderScript = RenderScript.create(this);
-        final Bitmap bitmapIn = BitmapFactory.decodeResource(getResources(), R.mipmap.sample1);
-        final Bitmap bitmapOut = Bitmap.createBitmap(bitmapIn.getWidth(), bitmapIn.getHeight(), Bitmap.Config.ARGB_8888);
+        mBitmapIn = BitmapFactory.decodeResource(getResources(), R.mipmap.sample);
+        mBitmapOut = Bitmap.createBitmap(mBitmapIn.getWidth(), mBitmapIn.getHeight(), Bitmap.Config.ARGB_8888);
 
-        mImageView.setImageBitmap(bitmapIn);
-        mAllocationIn = Allocation.createFromBitmap(mRenderScript, bitmapIn);
-        mAllocationOut = Allocation.createFromBitmap(mRenderScript, bitmapOut);
+        mImageView.setImageBitmap(mBitmapIn);
+        mAllocationIn = Allocation.createFromBitmap(mRenderScript, mBitmapIn);
+        mAllocationOut = Allocation.createFromBitmap(mRenderScript, mBitmapIn);
 
-        mScript = new ScriptC_invert(mRenderScript);
-        mInvertBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mScript.forEach_invert(mAllocationIn, mAllocationOut);
-                        mAllocationOut.copyTo(bitmapOut);
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                mImageView.setImageBitmap(bitmapOut);
-                            }
-                        });
-                    }
-                }).start();
-
-            }
-        });
+        mScriptInvert = new ScriptC_invert(mRenderScript);
 
         mScriptBlur = ScriptIntrinsicBlur.create(mRenderScript, Element.U8_4(mRenderScript));
-        mScriptBlur.setRadius(10f);
+        mScriptBlur.setRadius(BLUR_KERNEL_RADIUS);
 
-        mBlurBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                    final long start = System.currentTimeMillis();
+        mScriptBoxBlur = new ScriptC_boxblur(mRenderScript);
 
-                    mScriptBlur.setInput(mAllocationIn);
-                    mScriptBlur.forEach(mAllocationOut);
-                    mAllocationOut.copyTo(bitmapOut);
-                    final long stop = System.currentTimeMillis();
-
-                    runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mImageView.setImageBitmap(bitmapOut);
-                        Toast.makeText(RenderScriptActivity.this, (stop - start) + "ms", Toast.LENGTH_LONG).show();
-
-                    }
-                    });
-                    }
-                }).start();
-            }
-        });
+        mInvertBtn.setOnClickListener(this);
+        mBoxBlurBtn.setOnClickListener(this);
+        mGaussianBlurBtn.setOnClickListener(this);
 
     }
 
@@ -101,5 +74,47 @@ public class RenderScriptActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mRenderScript.destroy();
+    }
+
+    @Override
+    public void onClick(final View view) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final long start = System.currentTimeMillis();
+
+                switch (view.getId()) {
+                    case R.id.invert_btn:
+                        mScriptInvert.forEach_invert(mAllocationIn, mAllocationOut);
+                        break;
+
+                    case R.id.box_blur_btn:
+                        mScriptBoxBlur.set_input(mAllocationIn);
+                        mScriptBoxBlur.set_output(mAllocationOut);
+                        mScriptBoxBlur.set_radius((int) BLUR_KERNEL_RADIUS);
+                        mScriptBoxBlur.forEach_boxblur(mAllocationIn);
+
+                        break;
+                    case R.id.stack_blur_btn:
+                        break;
+                    case R.id.gaussian_blur_btn:
+                        mScriptBlur.setInput(mAllocationIn);
+                        mScriptBlur.forEach(mAllocationOut);
+                        break;
+                }
+
+                final long stop = System.currentTimeMillis();
+                mAllocationOut.copyTo(mBitmapOut);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mImageView.setImageBitmap(mBitmapOut);
+                        Toast.makeText(RenderScriptActivity.this, (stop - start) + "ms", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }).start();
+
     }
 }
--- app/src/main/rs/boxblur.rs
+++ app/src/main/rs/boxblur.rs
@@ -4,3 +4,28 @@
 
 #pragma rs_fp_relaxed
 
+rs_allocation input;
+rs_allocation output;
+int radius;
+
+void __attribute__((kernel)) boxblur(uchar4 in, uint32_t x, uint32_t y) {
+
+    float4 sum = 0;
+    uchar4 result;
+    int count = 0;
+    int kernel = (2 * radius + 1);
+
+    for (int i = -radius; i <= radius; i++) {
+        for (int j = -radius; j <= radius; j++) {
+            uchar4 temp = rsGetElementAt_uchar4(input, x + i, y + j);
+            sum += rsUnpackColor8888(temp);
+            count++;
+        }
+    }
+
+    sum = sum / count;
+    result = rsPackColorTo8888(sum);
+    result.a = 255;
+    rsSetElementAt_uchar4(output, result, x, y);
+}
+
