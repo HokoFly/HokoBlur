7005f6cd17af9ab273b6a0a977ee7a3dc08a4389
yuxfzju <243915316@qq.com>
2023-09-21 10:52:49 +0800

    Add android instrumented test

---
+++ hoko-blur/src/androidTest/java/com/hoko/blur/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.hoko.blur;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.hoko.blur", appContext.getPackageName());
+    }
+}
