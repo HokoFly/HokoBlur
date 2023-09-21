45dc6bbd238d11d69f6d619bcb1670cb58f27323
yuxfzju <243915316@qq.com>
2023-09-21 23:09:26 +0800

    Add android instrumented test

--- hoko-blur/src/androidTest/java/com/hoko/blur/ExampleInstrumentedTest.java
+++ hoko-blur/src/androidTest/java/com/hoko/blur/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.hoko.blur", appContext.getPackageName());
+        assertEquals("com.hoko.blur.test", appContext.getPackageName());
     }
 }
