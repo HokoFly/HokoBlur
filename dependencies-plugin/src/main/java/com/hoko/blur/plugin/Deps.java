d55b76223c14ab4d3f73534d55c95b13a050ad4b
yuxfzju <243915316@qq.com>
2023-12-18 18:43:29 +0800

    Use deps plugin manage release version

--- dependencies-plugin/src/main/java/com/hoko/blur/plugin/Deps.java
+++ dependencies-plugin/src/main/java/com/hoko/blur/plugin/Deps.java
@@ -19,13 +19,12 @@ public class Deps implements Plugin<Project> {
     public static final boolean renderscriptSupportModeEnabled = true;
     public static final JavaVersion javaVersion = JavaVersion.VERSION_17;
     public static final String ndkVersion = "26.1.10909125";
+    public static final String hokoBlurReleaseVersion = "1.5.3";
 
     public static final String appcompat = "androidx.appcompat:appcompat:1.4.0";
     public static final String junit = "junit:junit:4.13.2";
     public static final String androidTestJunit = "androidx.test.ext:junit:1.1.5";
     public static final String espressoCore = "androidx.test.espresso:espresso-core:3.5.1";
     public static final String androidAnnotation = "androidx.annotation:annotation:1.7.0";
-
-    public static final String androidGradlePlugin = "com.android.tools.build:gradle:8.1.2";
 
 }
