8ece412f05c44665c5b1a4bd0ab5baec8496848e
yuxfzju <243915316@qq.com>
2025-06-23 14:30:52 +0800

    Update AGP & ndk version to support 16KB

--- dependencies-plugin/src/main/java/com/hoko/blur/plugin/Deps.java
+++ dependencies-plugin/src/main/java/com/hoko/blur/plugin/Deps.java
@@ -11,14 +11,14 @@ public class Deps implements Plugin<Project> {
     }
 
     public static final int minSdkVersion = 21;
-    public static final int compileSdkVersion = 34;
-    public static final int targetSdkVersion = 34;
-    public static final String buildToolsVersion = "34.0.0";
-    public static final int renderscriptTargetApi = 34;
+    public static final int compileSdkVersion = 36;
+    public static final int targetSdkVersion = 36;
+    public static final String buildToolsVersion = "36.0.0";
+    public static final int renderscriptTargetApi = 36;
     public static final boolean renderscriptEnabled = true;
     public static final boolean renderscriptSupportModeEnabled = true;
     public static final JavaVersion javaVersion = JavaVersion.VERSION_17;
-    public static final String ndkVersion = "26.1.10909125";
+    public static final String ndkVersion = "28.1.13356709";
     public static final String hokoBlurReleaseVersion = "1.5.3";
 
     public static final String appcompat = "androidx.appcompat:appcompat:1.4.0";
